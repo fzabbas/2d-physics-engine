a13f73f4998f277e5e6c4fbbd81361696f1a3ae3
Farhana Abbas <farhana.z.abbas@gmail.com>
2021-10-28 12:12:12 -0400

    commented out game logic for now

--- game.js
+++ game.js
@@ -13,14 +13,14 @@ for (let addBody = 0; addBody < 10; addBody++) {
         let capsObj = new Capsule (x0, y0, x1, y1, r, m);
         capsObj.setPosition(100, 100);
         capsObj.color = "lightgreen"
-        capsObj.layer = 1;
+        // capsObj.layer = 1;
         randomObj.push(capsObj);
     }
     if (addBody%4 === 1) {
         let boxObj = new Box (x0, y0, x1, y1, r, m);
         boxObj.setPosition(200, 200);
         boxObj.color = "blue"
-        boxObj.layer = 2;
+        // boxObj.layer = 2;
         randomObj.push(boxObj);
 
     }
@@ -28,7 +28,7 @@ for (let addBody = 0; addBody < 10; addBody++) {
         let ballObj = new Ball(x0, y0, r, m);
         ballObj.setPosition(300, 300);
         ballObj.color = "red"
-        ballObj.layer = 3;
+        // ballObj.layer = 3;
         randomObj.push(ballObj);
 
     }
@@ -36,7 +36,7 @@ for (let addBody = 0; addBody < 10; addBody++) {
         let starObj = new Star(x0, y0, r+20, m);
         starObj.setPosition(400, 300);
         starObj.color = "orange"
-        starObj.layer = 4;
+        // starObj.layer = 4;
         randomObj.push(starObj);
 
     }
@@ -54,11 +54,11 @@ playerBall.color = "black"
 playerBall.maxSpeed = 3;
 
 function gameLogic() {
-    for (let i in randomObj){
-        if (collide(randomObj[i], playerBall)) {
-            // playerBall.remove();
-            randomObj[i].remove();
-            randomObj.splice(i,1);
-        }
-    }
+    // for (let i in randomObj){
+    //     if (collide(randomObj[i], playerBall)) {
+    //         // playerBall.remove();
+    //         randomObj[i].remove();
+    //         randomObj.splice(i,1);
+    //     }
+    // }
 }
